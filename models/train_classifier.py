19d270f5d6bf4fdb6c939f7b250167e18e31774f
Oliver Schillinger <oliver.schillinger@innogy.com>
2020-12-01 21:56:28 +0100

    Training and evaluation of random forest classifier implemented

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,103 @@
 import sys
+import re
+import pandas as pd
+import pickle
+from sqlalchemy import create_engine
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.pipeline import Pipeline
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet') # download for lemmatization
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Load messages and categories from SQLite DB
+    :param database_filepath: relative SQLite DB filepath
+    :return: X, Y, category_names: DataFrames with messages and categories and a list with category names
+    """
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql('messages', con=engine)
+    X = df[['message']]
+    Y = df.iloc[:, 4:]
+    categoriy_names = Y.columns
+    return X, Y, categoriy_names
 
 
 def tokenize(text):
-    pass
+    """Tokenize the provided text by
+            * removing punctuation
+            * tokenizing into words
+            * removing stopwords
+            * lemmatizing, including conversion to lowercase
+        """
+
+    # remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+
+    # tokenize into words
+    tokens = word_tokenize(text)
+
+    # remove stopwords
+    tokens = [t for t in tokens if t not in stopwords.words("english")]
+
+    # lemmatize, including conversion to lowercase
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(tok).lower().strip() for tok in tokens]
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Construct ML pipeline with count vectorization, TFIDF transformation and a MultiOuput RandomForest classifier
+    :return:
+    """
+    pipeline = Pipeline([
+        ('countVectorizer', CountVectorizer(tokenizer=tokenize, max_df=0.75, max_features=500)),
+        ('TFIDF', TfidfTransformer()),
+        ('MultiRandomForest', MultiOutputClassifier(RandomForestClassifier(n_estimators=100, min_samples_split=4), n_jobs=4))
+    ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Predict the test data with the given model and evalutate the result
+    :param model: ML pipeline
+    :param X_test: Test data messages
+    :param Y_test: Test data categories
+    :param category_names: category names of Y_test
+    :return: None
+    """
+    Y_pred = model.predict(X_test)
+    print(f"Model accuracy: {accuracy_score(Y_test, Y_pred):.2f}")
+    report = classification_report(Y_test, Y_pred, target_names=category_names, zero_division=0)
+    print(report)
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Pickle ML model to disk
+    :param model: ML pipeline
+    :param model_filepath: path to pickled file
+    :return: None
+    """
+    with open(model_filepath, 'wb') as outfile:
+        pickle.dump(model, outfile)
 
 
 def main():
@@ -32,10 +111,10 @@ def main():
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train["message"], Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test["message"], Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
